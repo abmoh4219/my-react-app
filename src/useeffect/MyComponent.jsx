2a9b7a8e0ae32bb5d284a1960cdbd5e1d28b1ddc
Abdelah <abdelahmohammed0919@gmail.com>
2025-02-01 10:29:25 +0300

    resizing the width and height using use Effect

--- src/useeffect/MyComponent.jsx
+++ src/useeffect/MyComponent.jsx
@@ -26,15 +26,32 @@
 */ 
 import React,{useState,useEffect} from "react";
 function  MyComponent () {
-    const [count,setCount]=useState(0);
+    const [width,setWidth]=useState(window.innerWidth);
+    const [height,setHeight]=useState(window.innerHeight);
+    useEffect(() => {
+        window.addEventListener("resize",handleResize);
+        console.log("EVENT LISTINER ADDED");
 
-    function addCount(){
-        setCount(c => count+1);
+        return() => {
+            window.removeEventListener("resize",handleResize);
+            console.log("EVENT LISTINER REMOVED");
+            
+        }
+        
+    },[]);
+
+    useEffect(() => {
+        document.title=`size: ${width} x ${height}`
+    },[width,height]);
+
+    function handleResize(){
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
     }
 
     return ( <>
-        <p>Count: {count}</p>
-        <button onClick={addCount}>Add</button>
+        <p>Window Width: {width}px</p>
+        <p>Window Height: {height}px</p>
     </> );
 }
  
